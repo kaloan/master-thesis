b7140cb5017b371a8a4c0c730dad49cc2a7f8630
Kaloian Stoilov <kaloan_sto@abv.bg>
2025-01-27 11:57:24 +0200

    Update basic c++

--- code/c++/SISUV/src/main.cpp
+++ code/c++/SISUV/src/main.cpp
@@ -3,7 +3,8 @@
 #include <cmath>
 #include <iostream>
 #include <iterator>
-#include <numeric>
+#include <tuple>
+// #include <numeric>
 // #include <functional>
 using namespace std;
 
@@ -70,15 +71,24 @@ auto concatenate(const std::array<Type, sizes> &...arrays) {
 
 // x and y
 constexpr auto padX = 6;
-// array<double, padX> xm;
-// array<double, padX> xp;
-// array<double, Nx> xb;
 array<double, Nx + 2 * padX> x;
 constexpr auto padY = 6;
 array<double, Ny + 2 * padY> y;
+// mdspan<double, extents<std::size_t, 1>> MDSPAN(x.data(), Nx + 2 * padX);
+array<array<double, x.size()>, y.size()> h1;
+array<array<double, x.size()>, y.size()> h2;
+array<array<double, x.size()>, y.size()> m1;
+array<array<double, x.size()>, y.size()> m2;
+array<array<double, x.size()>, y.size()> wwx;
+array<array<double, x.size()>, y.size()> wwy;
+array<array<double, x.size()>, y.size()> ax;
+array<array<double, x.size()>, y.size()> ay;
+array<array<double, x.size()>, y.size()> gs;
+// array<array<double, x.size()>, y.size()> wm;
+array<array<double, x.size() + 6>, y.size() + 6> w;
 
 template <typename T, size_t size>
-array<T, size> sum(array<T, size> arr1, array<T, size> arr2) {
+array<T, size> sum(const array<T, size> &arr1, const array<T, size> &arr2) {
   array<T, size> res;
   for (size_t i = 0; i < size; i++) {
     res[i] = arr1[i] + arr2[i];
@@ -86,10 +96,95 @@ array<T, size> sum(array<T, size> arr1, array<T, size> arr2) {
   return res;
 }
 
-template <typename T, size_t size> void printArray(array<T, size> arr) {
+template <typename T, size_t size> void printArray(const array<T, size> &arr) {
   std::copy(std::begin(arr), std::end(arr),
             std::ostream_iterator<T>(std::cout, ", "));
   std::cout << std::endl;
+}
+
+template <typename T, size_t rows, size_t cols>
+void printMatrix(const array<array<T, cols>, rows> &arr) {
+  for (auto row : arr) {
+    printArray(row);
+  }
+}
+
+template <typename T, size_t rows, size_t cols>
+void matrixContinuation(const array<array<T, cols>, rows> &mat) {
+  for (size_t j = 0; j < x.size(); j++) {
+    mat[0][j + 3] = mat[1][j + 3] = mat[2][j + 3] = mat[3][j + 3];
+    mat[y.size() + 3][j + 3] = mat[y.size() + 4][j + 3] =
+        mat[y.size() + 5][j + 3] = mat[y.size() + 2][j + 3];
+  }
+  for (size_t i = 0; i < y.size(); i++) {
+    mat[i + 3][0] = mat[i + 3][1] = mat[i + 3][2] = mat[i + 3][3];
+    mat[i + 3][x.size() + 3] = mat[i + 3][x.size() + 4] =
+        mat[i + 3][x.size() + 5] = mat[i + 3][x.size() + 2];
+  }
+}
+
+tuple<double, double, double> stencils(const double &v1, const double &v2,
+                                       const double &v3, const double &v4,
+                                       const double &v5) {
+  auto f1x = v1 / 3 - 7 * v2 / 6 + 11 * v3 / 6;
+  auto f2x = -v2 / 6 + 5 * v3 / 6 + v4 / 3;
+  auto f3x = v3 / 3 + 5 * v4 / 6 - v5 / 6;
+  // smoothness of stencils
+  auto S1 = 13. / 12 * pow((v1 - 2 * v2 + v3), 2) +
+            1. / 4 * pow((v1 - 4 * v2 + 3 * v3), 2);
+  auto S2 = 13. / 12 * pow((v2 - 2 * v3 + v4), 2) + 1. / 4 * pow((v2 - v4), 2);
+  auto S3 = 13. / 12 * pow((v3 - 2 * v4 + v5), 2) +
+            1. / 4 * pow((3 * v3 - 4 * v4 + v5), 2);
+  // return make_tuple(S1, S2, S3);
+  return {S1, S2, S3};
+}
+
+double derf(const double &v1, const double &v2, const double &v3,
+            const double &v4, const double &v5, const double &eps1) {
+  auto f1x = v1 / 3 - 7 * v2 / 6 + 11 * v3 / 6;
+  auto f2x = -v2 / 6 + 5 * v3 / 6 + v4 / 3;
+  auto f3x = v3 / 3 + 5 * v4 / 6 - v5 / 6;
+  // smoothness of stencils
+  auto S1 = 13. / 12 * pow((v1 - 2 * v2 + v3), 2) +
+            1. / 4 * pow((v1 - 4 * v2 + 3 * v3), 2);
+  auto S2 = 13. / 12 * pow((v2 - 2 * v3 + v4), 2) + 1. / 4 * pow((v2 - v4), 2);
+  auto S3 = 13. / 12 * pow((v3 - 2 * v4 + v5), 2) +
+            1. / 4 * pow((3 * v3 - 4 * v4 + v5), 2);
+  // find the maximum of vi's element-wise
+  // auto findMaxList = {v1 * v1, v2 * v2, v3 * v3, v4 * v4, v5 * v5};
+  // auto maxv2 = *max_element(findMaxList.begin(), findMaxList.end());
+  // auto eps1 = 1e-6 * maxv2 + 1e-99;
+  // weights
+  auto a1 = .1 / pow((S1 + eps1), 2);
+  auto a2 = .6 / pow((S2 + eps1), 2);
+  auto a3 = .3 / pow((S3 + eps1), 2);
+  auto w1 = a1 / (a1 + a2 + a3);
+  auto w2 = a2 / (a1 + a2 + a3);
+  auto w3 = a3 / (a1 + a2 + a3);
+  return w1 * f1x + w2 * f2x + w3 * f3x;
+}
+
+template <size_t n>
+array<double, n> derf(array<double, n> v1, array<double, n> v2,
+                      array<double, n> v3, array<double, n> v4,
+                      array<double, n> v5) {
+  auto maxv2 = 0.;
+  for (size_t i = 0; i < n; i++) {
+    auto findMaxList = {v1[i] * v1[i], v2[i] * v2[i], v3[i] * v3[i],
+                        v4[i] * v4[i], v5[i] * v5[i]};
+    auto maxv2Possible = *max_element(findMaxList.begin(), findMaxList.end());
+    maxv2 = max(maxv2, maxv2Possible);
+  }
+  auto eps1 = 1e-6 * maxv2 + 1e-99;
+  array<double, n> res;
+  for (size_t i = 0; i < n; i++) {
+    res[i] = derf(v1[i], v2[i], v3[i], v4[i], v5[i], eps1);
+  }
+  return res;
+}
+
+template <size_t rows, size_t cols> array<array<double, cols>, rows> a() {
+  return {};
 }
 
 int main(int argc, char *argv[]) {
@@ -105,4 +200,41 @@ int main(int argc, char *argv[]) {
   printArray(x);
   cout << "YYYYYY:";
   printArray(y);
+  // compute the partial derivatives
+  for (size_t i = 0; i < y.size(); i++) {
+    for (size_t j = 0; j < x.size(); j++) {
+      h1[i][j] = Gamma * x[j] - alpha * nu * (1 - x[j]) * y[i];
+      h2[i][j] = mu * y[i] - Beta * x[j] * (1 - y[i]);
+      m1[i][j] = alpha * nu * k * umax * (1 - x[j]) * y[i];
+      m2[i][j] = Beta * k * umax * x[j] * (1 - y[i]);
+
+      // find maximum range of gridpoints
+      // where a_x must be chosen according to LLF
+      wwx[i][j] = (h1[i][j] * (h1[i][j] + m1[i][j])) < 1e-99;
+      ax[i][j] = max(abs(h1[i][j]), abs(h1[i][j] + m1[i][j]));
+
+      // find maximum range of gridpoints
+      // where a_y must be chosen according to LLF
+      wwy[i][j] = (h2[i][j] * (h2[i][j] + m2[i][j])) < 1e-99;
+      ay[i][j] = max(abs(h2[i][j]), abs(h2[i][j] + m2[i][j]));
+
+      gs[i][j] = x[j] - Imax;
+      // Initial guess
+      w[i + 3][j + 3] = (sin((x[j] - .5) * M_PI * 2)) * 1e-6 + gs[i][j];
+    }
+  }
+  matrixContinuation(w);
+  // printMatrix(w);
+  auto err0 = 1.;
+  while (err0 > errtol) {
+    // Heun's predictor-corrector method
+    array<array<double, x.size() + 6>, y.size()> wy;
+    array<array<double, x.size()>, y.size() + 6> wx;
+    for (int i = 0; i < y.size(); i++) {
+      for (int j = 0; j < x.size(); j++) {
+        wy[i][j + 3] = w[i + 1][j + 3] - w[i][j + 3];
+        wx[i + 3][j] = w[i + 3][j + 1] - w[i + 3][j];
+      }
+    }
+  }
 }
