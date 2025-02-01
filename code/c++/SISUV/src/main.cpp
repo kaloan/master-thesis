36eadc320cb07c8257c1e521d8fac733511b23fa
Kaloian Stoilov <kaloan_sto@abv.bg>
2025-02-01 18:50:19 +0200

    Continue with code

--- code/c++/SISUV/src/main.cpp
+++ code/c++/SISUV/src/main.cpp
@@ -8,6 +8,13 @@
 // #include <functional>
 using namespace std;
 
+template <size_t rows, size_t cols> struct derivativeApproximations {
+  array<array<double, cols - 6>, rows - 6> fxp;
+  array<array<double, cols - 6>, rows - 6> fxm;
+  array<array<double, cols - 6>, rows - 6> fyp;
+  array<array<double, cols - 6>, rows - 6> fym;
+};
+
 // parameters from Goldberg, Agusto 2021
 // human
 constexpr auto Gamma = 1. / 14;
@@ -69,6 +76,19 @@ auto concatenate(const std::array<Type, sizes> &...arrays) {
   return result;
 }
 
+template <typename T, size_t rows, size_t cols>
+array<array<T, rows>, cols> transpose(const array<array<T, cols>, rows> &mat) {
+  array<array<T, rows>, cols> transposed;
+
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      transposed[j][i] = mat[i][j];
+    }
+  }
+
+  return transposed;
+}
+
 // x and y
 constexpr auto padX = 6;
 array<double, Nx + 2 * padX> x;
@@ -94,6 +114,26 @@ array<T, size> sum(const array<T, size> &arr1, const array<T, size> &arr2) {
     res[i] = arr1[i] + arr2[i];
   }
   return res;
+}
+
+template <typename T, size_t size>
+array<T, size> halfSum(const array<T, size> &arr1, const array<T, size> &arr2) {
+  array<T, size> res;
+  for (size_t i = 0; i < size; i++) {
+    res[i] = (arr1[i] + arr2[i]) / 2.;
+  }
+  return res;
+}
+
+template <size_t cols, size_t rows>
+double find_maxv2(const array<array<double, cols>, rows> &matrix) {
+  auto maxv2 = 0.;
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < cols; j++) {
+      maxv2 = max(maxv2, matrix[i][j] * matrix[i][j]);
+    }
+  }
+  return maxv2;
 }
 
 template <typename T, size_t size> void printArray(const array<T, size> &arr) {
@@ -130,11 +170,12 @@ tuple<double, double, double> stencils(const double &v1, const double &v2,
   auto f2x = -v2 / 6 + 5 * v3 / 6 + v4 / 3;
   auto f3x = v3 / 3 + 5 * v4 / 6 - v5 / 6;
   // smoothness of stencils
-  auto S1 = 13. / 12 * pow((v1 - 2 * v2 + v3), 2) +
-            1. / 4 * pow((v1 - 4 * v2 + 3 * v3), 2);
-  auto S2 = 13. / 12 * pow((v2 - 2 * v3 + v4), 2) + 1. / 4 * pow((v2 - v4), 2);
-  auto S3 = 13. / 12 * pow((v3 - 2 * v4 + v5), 2) +
-            1. / 4 * pow((3 * v3 - 4 * v4 + v5), 2);
+  auto S1 = 13. / 12 * (v1 - 2 * v2 + v3) * (v1 - 2 * v2 + v3) +
+            1. / 4 * (v1 - 4 * v2 + 3 * v3) * (v1 - 4 * v2 + 3 * v3);
+  auto S2 = 13. / 12 * (v2 - 2 * v3 + v4) * (v2 - 2 * v3 + v4) +
+            1. / 4 * (v2 - v4) * (v2 - v4);
+  auto S3 = 13. / 12 * (v3 - 2 * v4 + v5) * (v3 - 2 * v4 + v5) +
+            1. / 4 * (3 * v3 - 4 * v4 + v5) * (3 * v3 - 4 * v4 + v5);
   // return make_tuple(S1, S2, S3);
   return {S1, S2, S3};
 }
@@ -145,19 +186,20 @@ double derf(const double &v1, const double &v2, const double &v3,
   auto f2x = -v2 / 6 + 5 * v3 / 6 + v4 / 3;
   auto f3x = v3 / 3 + 5 * v4 / 6 - v5 / 6;
   // smoothness of stencils
-  auto S1 = 13. / 12 * pow((v1 - 2 * v2 + v3), 2) +
-            1. / 4 * pow((v1 - 4 * v2 + 3 * v3), 2);
-  auto S2 = 13. / 12 * pow((v2 - 2 * v3 + v4), 2) + 1. / 4 * pow((v2 - v4), 2);
-  auto S3 = 13. / 12 * pow((v3 - 2 * v4 + v5), 2) +
-            1. / 4 * pow((3 * v3 - 4 * v4 + v5), 2);
+  auto S1 = 13. / 12 * (v1 - 2 * v2 + v3) * (v1 - 2 * v2 + v3) +
+            1. / 4 * (v1 - 4 * v2 + 3 * v3) * (v1 - 4 * v2 + 3 * v3);
+  auto S2 = 13. / 12 * (v2 - 2 * v3 + v4) * (v2 - 2 * v3 + v4) +
+            1. / 4 * (v2 - v4) * (v2 - v4);
+  auto S3 = 13. / 12 * (v3 - 2 * v4 + v5) * (v3 - 2 * v4 + v5) +
+            1. / 4 * (3 * v3 - 4 * v4 + v5) * (3 * v3 - 4 * v4 + v5);
   // find the maximum of vi's element-wise
   // auto findMaxList = {v1 * v1, v2 * v2, v3 * v3, v4 * v4, v5 * v5};
   // auto maxv2 = *max_element(findMaxList.begin(), findMaxList.end());
   // auto eps1 = 1e-6 * maxv2 + 1e-99;
   // weights
-  auto a1 = .1 / pow((S1 + eps1), 2);
-  auto a2 = .6 / pow((S2 + eps1), 2);
-  auto a3 = .3 / pow((S3 + eps1), 2);
+  auto a1 = .1 / ((S1 + eps1) * (S1 + eps1));
+  auto a2 = .6 / ((S2 + eps1) * (S2 + eps1));
+  auto a3 = .3 / ((S3 + eps1) * (S3 + eps1));
   auto w1 = a1 / (a1 + a2 + a3);
   auto w2 = a2 / (a1 + a2 + a3);
   auto w3 = a3 / (a1 + a2 + a3);
@@ -165,17 +207,9 @@ double derf(const double &v1, const double &v2, const double &v3,
 }
 
 template <size_t n>
-array<double, n> derf(array<double, n> v1, array<double, n> v2,
-                      array<double, n> v3, array<double, n> v4,
-                      array<double, n> v5) {
-  auto maxv2 = 0.;
-  for (size_t i = 0; i < n; i++) {
-    auto findMaxList = {v1[i] * v1[i], v2[i] * v2[i], v3[i] * v3[i],
-                        v4[i] * v4[i], v5[i] * v5[i]};
-    auto maxv2Possible = *max_element(findMaxList.begin(), findMaxList.end());
-    maxv2 = max(maxv2, maxv2Possible);
-  }
-  auto eps1 = 1e-6 * maxv2 + 1e-99;
+array<double, n> derf(const array<double, n> &v1, const array<double, n> &v2,
+                      const array<double, n> &v3, const array<double, n> &v4,
+                      const array<double, n> &v5, const double &eps1) {
   array<double, n> res;
   for (size_t i = 0; i < n; i++) {
     res[i] = derf(v1[i], v2[i], v3[i], v4[i], v5[i], eps1);
@@ -183,8 +217,84 @@ array<double, n> derf(array<double, n> v1, array<double, n> v2,
   return res;
 }
 
-template <size_t rows, size_t cols> array<array<double, cols>, rows> a() {
-  return {};
+template <typename T, size_t rows, size_t cols>
+derivativeApproximations<rows, cols>
+derf(const array<array<T, cols>, rows> &mat) {
+  array<array<double, cols>, rows - 6> wx;
+  array<array<double, cols - 6>, rows> wy;
+  for (int i = 0; i < rows - 6; i++) {
+    for (int j = 0; j < cols - 6; j++) {
+      wx[i][j] = (w[i + 3][j + 1] - w[i + 3][j]) / dx;
+      wy[i][j] = (w[i + 1][j + 3] - w[i][j + 3]) / dy;
+    }
+  }
+
+  derivativeApproximations<rows, cols> result;
+
+  auto epsX = find_maxv2(wx);
+  for (size_t i = 0; i < wy.size() - 4; i++) {
+    result.fym[i] =
+        derf(wy[i], wy[i + 1], wy[i + 2], wy[i + 3], wy[i + 4], epsX);
+  }
+  for (size_t i = 4; i < wy.size(); i++) {
+    result.fyp[i - 4] =
+        derf(wy[i], wy[i - 1], wy[i - 2], wy[i - 3], wy[i - 4], epsX);
+  }
+
+  auto epsY = find_maxv2(wy);
+  auto transposedWx = transpose(wx);
+  array<array<double, rows - 6>, cols - 6> transposedFx;
+  for (size_t i = 0; i < transposedWx.size() - 4; i++) {
+    transposedFx[i] =
+        derf(transposedWx[i], transposedWx[i + 1], transposedWx[i + 2],
+             transposedWx[i + 3], transposedWx[i + 4], epsY);
+  }
+  result.fxm = transpose(transposedFx);
+  for (size_t i = 4; i < transposedWx.size(); i++) {
+    transposedFx[i - 4] =
+        derf(transposedWx[i], transposedWx[i - 1], transposedWx[i - 2],
+             transposedWx[i - 3], transposedWx[i - 4], epsY);
+  }
+  result.fxp = transpose(transposedFx);
+
+  return result;
+}
+
+template <size_t rows, size_t cols>
+array<array<double, cols>, rows>
+hamiltonian(const array<array<double, cols>, rows> &xp,
+            const array<array<double, cols>, rows> &xm,
+            const array<array<double, cols>, rows> &yp,
+            const array<array<double, cols>, rows> &ym) {
+  array<array<double, cols>, rows> hamiltonian;
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      auto chooseAx = wwx[i][j] * (xp[i][j] * xm[i][j] < 1e-99);
+      auto interm =
+          h1[i][j] + (m1[i][j] * (xp[i][j] + xm[i][j]) / 2 > 1e-99) * m1[i][j];
+      // upwind outside ww1 Osher & Shu (2.11) - (2.11) 1st case
+      hamiltonian[i][j] +=
+          (1 - chooseAx) * ((interm < 1e-99) * interm * xp[i][j] +
+                            (interm > 1e-99) * interm * xm[i][j]);
+      hamiltonian[i][j] += chooseAx * interm * (xp[i][j] + xm[i][j]) /
+                           2; // Osher & Shu (2.11) -2nd case
+      hamiltonian[i][j] -= chooseAx * ax[i][j] * (xp[i][j] - xm[i][j]) / 2;
+
+      auto chooseAy = wwy[i][j] * (yp[i][j] * ym[i][j] < 1e-99);
+      auto interm2 =
+          h2[i][j] + (m2[i][j] * (yp[i][j] + ym[i][j]) / 2 > 1e-99) * m2[i][j];
+      // upwind outside ww1 Osher & Shu (2.11) - (2.11) 1st case
+      hamiltonian[i][j] +=
+          (1 - chooseAy) * ((interm2 < 1e-99) * interm2 * yp[i][j] +
+                            (interm2 > 1e-99) * interm2 * ym[i][j]);
+      hamiltonian[i][j] += chooseAy * interm2 * (yp[i][j] + ym[i][j]) /
+                           2; // Osher & Shu (2.11) -2nd case
+      hamiltonian[i][j] -= chooseAy * ay[i][j] * (yp[i][j] - ym[i][j]) / 2;
+    }
+  }
+
+  return hamiltonian;
 }
 
 int main(int argc, char *argv[]) {
@@ -224,17 +334,39 @@ int main(int argc, char *argv[]) {
     }
   }
   matrixContinuation(w);
+  auto wnew0 = w;
+  auto wnew1 = w;
   // printMatrix(w);
   auto err0 = 1.;
   while (err0 > errtol) {
     // Heun's predictor-corrector method
-    array<array<double, x.size() + 6>, y.size()> wy;
-    array<array<double, x.size()>, y.size() + 6> wx;
-    for (int i = 0; i < y.size(); i++) {
-      for (int j = 0; j < x.size(); j++) {
-        wy[i][j + 3] = w[i + 1][j + 3] - w[i][j + 3];
-        wx[i + 3][j] = w[i + 3][j + 1] - w[i + 3][j];
+    auto firstApprox = derf(w);
+    auto hamilton = hamiltonian(firstApprox.fxm, firstApprox.fxp,
+                                firstApprox.fym, firstApprox.fyp);
+    for (size_t i = 0; i < y.size(); i++) {
+      for (size_t j = 0; j < x.size(); j++) {
+        wnew0[i + 3][j + 3] = (1 - ll * dt) * w[i + 3][j + 3] - hamilton[i][j];
       }
     }
+    matrixContinuation(wnew0);
+
+    auto secondApprox = derf(wnew0);
+    hamilton = hamiltonian(secondApprox.fxm, secondApprox.fxp, secondApprox.fym,
+                           secondApprox.fyp);
+    for (size_t i = 0; i < y.size(); i++) {
+      for (size_t j = 0; j < x.size(); j++) {
+        wnew1[i + 3][j + 3] =
+            (1 - ll * dt) * wnew0[i + 3][j + 3] - hamilton[i][j];
+      }
+    }
+    matrixContinuation(wnew1);
+    auto wnew = halfSum(wnew0, wnew1);
+    err0 = 0;
+    for (size_t i = 0; i < y.size(); i++) {
+      for (size_t j = 0; j < x.size(); j++) {
+        err0 = max(err0, abs(w[i + 3][j + 3] - wnew[i + 3][j + 3]));
+      }
+    }
+    w = wnew;
   }
 }
