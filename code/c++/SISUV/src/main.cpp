82c5137bbc156352d0db4d447c79adaa19244e4c
Kaloian Stoilov <kaloan_sto@abv.bg>
2025-02-06 11:43:46 +0200

    4 hours debugging for line 388 flipped parameters

--- code/c++/SISUV/src/main.cpp
+++ code/c++/SISUV/src/main.cpp
@@ -7,8 +7,6 @@
 #include <iterator>
 #include <tuple>
 #include <vector>
-// #include <numeric>
-// #include <functional>
 using namespace std;
 
 template <size_t rows, size_t cols> struct derivativeApproximations {
@@ -16,26 +14,17 @@ template <size_t rows, size_t cols> struct derivativeApproximations {
   array<array<double, cols - 6>, rows - 6> fxm;
   array<array<double, cols - 6>, rows - 6> fyp;
   array<array<double, cols - 6>, rows - 6> fym;
-
-  derivativeApproximations() {
-    array<double, cols - 6> zeroFill;
-    zeroFill.fill(0);
-    fxp.fill(zeroFill);
-    fxm.fill(zeroFill);
-    fyp.fill(zeroFill);
-    fym.fill(zeroFill);
-  }
 };
 
 // parameters from Goldberg, Agusto 2021
 // human
-constexpr auto Gamma = 1. / 14;
+constexpr auto Gamma = 1. / 14.;
 // constexpr auto alpha = 0.082*0.5*exp(-1/3); // Botsuana
-const auto alpha = 0.241 * 0.5 * exp(-1. / 3); // Zimbabwe
+const auto alpha = 0.241 * 0.5 * exp(-1. / 3.); // Zimbabwe
 // mosquito
 // beta = 0.082*0.1; mu=1/30; // Botsuana
 constexpr auto Beta = 0.241 * 0.1; // Zimbabwe
-constexpr auto mu = 1. / 10;       // Zimbabwe
+constexpr auto mu = 1. / 10.;      // Zimbabwe
 
 // ratio mosquito : human
 constexpr auto nu = 10.;
@@ -54,11 +43,10 @@ const auto Is =
      Beta * Gamma * (1 - k * umax));
 
 // predefine
-constexpr auto ntim = 650.;
 constexpr auto errtol = 1e-10;
 
 // lambda - lipschitz constant for the system
-const auto ll1 = 2 * (alpha * alpha * nu * nu + Beta * Beta + mu * mu);
+const auto ll1 = 2. * (alpha * alpha * nu * nu + Beta * Beta + mu * mu);
 const auto ll2 = sqrt(ll1);
 const auto ll = 1.05 * ll2;
 
@@ -68,7 +56,7 @@ const array<double, 2> mpd = {1 + Gamma + alpha * nu * (1 + k * umax),
 // mpd = mpd + 1; ????
 
 // numerical space step
-constexpr auto Nx = 41;
+constexpr auto Nx = 150;
 constexpr auto dx = Imax / (Nx - 1);
 constexpr auto Ny = 100;
 constexpr auto dy = ymax / (Ny - 1);
@@ -93,8 +81,8 @@ template <typename T, size_t rows, size_t cols>
 array<array<T, rows>, cols> transpose(const array<array<T, cols>, rows> &mat) {
   array<array<T, rows>, cols> transposed;
 
-  for (size_t i = 0; i < rows; i++) {
-    for (size_t j = 0; j < cols; j++) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
       transposed[j][i] = mat[i][j];
     }
   }
@@ -121,7 +109,7 @@ array<array<double, x.size() + 6>, y.size() + 6> w;
 template <typename T, size_t size>
 array<T, size> sum(const array<T, size> &arr1, const array<T, size> &arr2) {
   array<T, size> res;
-  for (size_t i = 0; i < size; i++) {
+  for (size_t i = 0; i < size; ++i) {
     res[i] = arr1[i] + arr2[i];
   }
   return res;
@@ -130,9 +118,9 @@ array<T, size> sum(const array<T, size> &arr1, const array<T, size> &arr2) {
 template <typename T, size_t size>
 array<T, size> halfSum(const array<T, size> &arr1, const array<T, size> &arr2) {
   array<T, size> res;
-  for (size_t i = 0; i < size; i++) {
-    // res[i] = (arr1[i] + arr2[i]) / 2.;
-    res[i] = arr1[i] / 2. + arr2[i] / 2.;
+  for (size_t i = 0; i < size; ++i) {
+    res[i] = (arr1[i] + arr2[i]) / 2.;
+    // res[i] = arr1[i] / 2. + arr2[i] / 2.;
   }
   return res;
 }
@@ -141,7 +129,7 @@ template <typename T, size_t rows, size_t cols>
 array<array<T, cols>, rows> halfSum(const array<array<T, cols>, rows> &mat1,
                                     const array<array<T, cols>, rows> &mat2) {
   array<array<T, cols>, rows> res;
-  for (size_t i = 0; i < rows; i++) {
+  for (size_t i = 0; i < rows; ++i) {
     res[i] = halfSum(mat1[i], mat2[i]);
   }
   return res;
@@ -150,8 +138,8 @@ array<array<T, cols>, rows> halfSum(const array<array<T, cols>, rows> &mat1,
 template <size_t cols, size_t rows>
 double find_maxv2(const array<array<double, cols>, rows> &matrix) {
   auto maxv2 = 0.;
-  for (size_t i = 0; i < rows; i++) {
-    for (size_t j = 0; j < cols; j++) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
       maxv2 = max(maxv2, matrix[i][j] * matrix[i][j]);
     }
   }
@@ -163,8 +151,8 @@ array<array<double, cols>, rows>
 max(const array<array<double, cols>, rows> &mat1,
     const array<array<double, cols>, rows> &mat2) {
   array<array<double, cols>, rows> result;
-  for (size_t i = 0; i < rows; i++) {
-    for (size_t j = 0; j < cols; j++) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
       result[i][j] = max(mat1[i][j], mat2[i][j]);
     }
   }
@@ -186,15 +174,23 @@ void printMatrix(const array<array<T, cols>, rows> &arr) {
 
 template <typename T, size_t rows, size_t cols>
 void matrixContinuation(array<array<T, cols>, rows> &mat) {
-  for (size_t j = 0; j < x.size(); j++) {
+  for (size_t j = 0; j < x.size(); ++j) {
     mat[0][j + 3] = mat[1][j + 3] = mat[2][j + 3] = mat[3][j + 3];
     mat[y.size() + 3][j + 3] = mat[y.size() + 4][j + 3] =
         mat[y.size() + 5][j + 3] = mat[y.size() + 2][j + 3];
   }
-  for (size_t i = 0; i < y.size(); i++) {
+  for (size_t i = 0; i < y.size(); ++i) {
     mat[i + 3][0] = mat[i + 3][1] = mat[i + 3][2] = mat[i + 3][3];
     mat[i + 3][x.size() + 3] = mat[i + 3][x.size() + 4] =
         mat[i + 3][x.size() + 5] = mat[i + 3][x.size() + 2];
+  }
+  for (size_t i = 0; i < 3; ++i) {
+    for (size_t j = 0; j < 3; ++j) {
+      mat[i][j] = mat[3][3];
+      mat[rows - 3 + i][j] = mat[rows - 3][3];
+      mat[i][cols - 3 + j] = mat[3][cols - 3];
+      mat[rows - 3 + i][cols - 3 + j] = mat[rows - 3][cols - 3];
+    }
   }
 }
 
@@ -221,7 +217,7 @@ double derf(const double &v1, const double &v2, const double &v3,
             1. / 4. * (v2 - v4) * (v2 - v4);
   auto S3 = 13. / 12. * (v3 - 2. * v4 + v5) * (v3 - 2. * v4 + v5) +
             1. / 4. * (3. * v3 - 4. * v4 + v5) * (3. * v3 - 4. * v4 + v5);
-  // find the maximum of vi's element-wise
+  // find the maximum of vi's
   auto maxv2 = max({v1 * v1, v2 * v2, v3 * v3, v4 * v4, v5 * v5});
   auto eps1 = 1e-6 * maxv2 + 1e-99;
   // weights
@@ -242,7 +238,7 @@ array<double, n> derf(const array<double, n> &v1, const array<double, n> &v2,
                       const array<double, n> &v3, const array<double, n> &v4,
                       const array<double, n> &v5) {
   array<double, n> res;
-  for (size_t i = 0; i < n; i++) {
+  for (size_t i = 0; i < n; ++i) {
     res[i] = derf(v1[i], v2[i], v3[i], v4[i], v5[i]);
   }
   return res;
@@ -253,41 +249,41 @@ derivativeApproximations<rows, cols>
 derf(const array<array<T, cols>, rows> &mat) {
   array<array<double, cols - 1>, rows - 6> wx;
   array<array<double, cols - 6>, rows - 1> wy;
-  for (size_t i = 0; i < rows - 6; i++) {
-    for (size_t j = 0; j < cols - 1; j++) {
+  for (size_t i = 0; i < rows - 6; ++i) {
+    for (size_t j = 0; j < cols - 1; ++j) {
       wx[i][j] = (mat[i + 3][j + 1] - mat[i + 3][j]) / dx;
     }
   }
-  for (size_t i = 0; i < rows - 1; i++) {
-    for (size_t j = 0; j < cols - 6; j++) {
+  for (size_t i = 0; i < rows - 1; ++i) {
+    for (size_t j = 0; j < cols - 6; ++j) {
       wy[i][j] = (mat[i + 1][j + 3] - mat[i][j + 3]) / dy;
     }
   }
 
   derivativeApproximations<rows, cols> result;
 
-  for (size_t i = 0; i < rows - 6; i++) {
-    for (size_t j = 0; j < cols - 6; j++) {
+  for (size_t i = 0; i < rows - 6; ++i) {
+    for (size_t j = 0; j < cols - 6; ++j) {
       result.fxm[i][j] = derf(wx[i][j], wx[i][j + 1], wx[i][j + 2],
                               wx[i][j + 3], wx[i][j + 4]);
     }
   }
 
-  for (size_t i = 0; i < rows - 6; i++) {
-    for (size_t j = 0; j < cols - 6; j++) {
+  for (size_t i = 0; i < rows - 6; ++i) {
+    for (size_t j = 0; j < cols - 6; ++j) {
       result.fxp[i][j] = derf(wx[i][j + 5], wx[i][j + 4], wx[i][j + 3],
                               wx[i][j + 2], wx[i][j + 1]);
     }
   }
 
-  for (size_t i = 0; i < rows - 6; i++) {
-    for (size_t j = 0; j < cols - 6; j++) {
+  for (size_t i = 0; i < rows - 6; ++i) {
+    for (size_t j = 0; j < cols - 6; ++j) {
       result.fym[i][j] = derf(wy[i][j], wy[i + 1][j], wy[i + 2][j],
                               wy[i + 3][j], wy[i + 4][j]);
     }
   }
-  for (size_t i = 0; i < rows - 6; i++) {
-    for (size_t j = 0; j < cols - 6; j++) {
+  for (size_t i = 0; i < rows - 6; ++i) {
+    for (size_t j = 0; j < cols - 6; ++j) {
       result.fyp[i][j] = derf(wy[i + 5][j], wy[i + 4][j], wy[i + 3][j],
                               wy[i + 2][j], wy[i + 1][j]);
     }
@@ -304,29 +300,29 @@ hamiltonian(const array<array<double, cols>, rows> &xp,
             const array<array<double, cols>, rows> &ym) {
   array<array<double, cols>, rows> hamiltonian;
 
-  for (size_t i = 0; i < rows; i++) {
-    for (size_t j = 0; j < cols; j++) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
       auto chooseAx = wwx[i][j] * (xp[i][j] * xm[i][j] < 1e-99);
       auto interm =
-          h1[i][j] + (m1[i][j] * (xp[i][j] + xm[i][j]) / 2 > 1e-99) * m1[i][j];
+          h1[i][j] + (m1[i][j] * (xp[i][j] + xm[i][j]) / 2. > 1e-99) * m1[i][j];
       // upwind outside ww1 Osher & Shu (2.11) - (2.11) 1st case
       hamiltonian[i][j] =
           (1 - chooseAx) * ((interm < 1e-99) * interm * xp[i][j] +
                             (interm > 1e-99) * interm * xm[i][j]);
       hamiltonian[i][j] += chooseAx * interm * (xp[i][j] + xm[i][j]) /
-                           2; // Osher & Shu (2.11) -2nd case
-      hamiltonian[i][j] -= chooseAx * ax[i][j] * (xp[i][j] - xm[i][j]) / 2;
+                           2.; // Osher & Shu (2.11) -2nd case
+      hamiltonian[i][j] -= chooseAx * ax[i][j] * (xp[i][j] - xm[i][j]) / 2.;
 
       auto chooseAy = wwy[i][j] * (yp[i][j] * ym[i][j] < 1e-99);
       auto interm2 =
-          h2[i][j] + (m2[i][j] * (yp[i][j] + ym[i][j]) / 2 > 1e-99) * m2[i][j];
+          h2[i][j] + (m2[i][j] * (yp[i][j] + ym[i][j]) / 2. > 1e-99) * m2[i][j];
       // upwind outside ww1 Osher & Shu (2.11) - (2.11) 1st case
       hamiltonian[i][j] +=
           (1 - chooseAy) * ((interm2 < 1e-99) * interm2 * yp[i][j] +
                             (interm2 > 1e-99) * interm2 * ym[i][j]);
       hamiltonian[i][j] += chooseAy * interm2 * (yp[i][j] + ym[i][j]) /
-                           2; // Osher & Shu (2.11) -2nd case
-      hamiltonian[i][j] -= chooseAy * ay[i][j] * (yp[i][j] - ym[i][j]) / 2;
+                           2.; // Osher & Shu (2.11) -2nd case
+      hamiltonian[i][j] -= chooseAy * ay[i][j] * (yp[i][j] - ym[i][j]) / 2.;
     }
   }
 
@@ -342,13 +338,10 @@ int main(int argc, char *argv[]) {
            [n = 0]() mutable { return (-padY + n++) * dy; });
   y[padY] = 0;
   y[padY + Ny - 1] = ymax;
-  cout << "XXXXXX:";
-  printArray(x);
-  cout << "YYYYYY:";
-  printArray(y);
+
   // compute the partial derivatives
-  for (size_t i = 0; i < y.size(); i++) {
-    for (size_t j = 0; j < x.size(); j++) {
+  for (size_t i = 0; i < y.size(); ++i) {
+    for (size_t j = 0; j < x.size(); ++j) {
       h1[i][j] = Gamma * x[j] - alpha * nu * (1 - x[j]) * y[i];
       h2[i][j] = mu * y[i] - Beta * x[j] * (1 - y[i]);
       m1[i][j] = alpha * nu * k * umax * (1 - x[j]) * y[i];
@@ -366,7 +359,7 @@ int main(int argc, char *argv[]) {
 
       gs[i][j] = x[j] - Imax;
       // Initial guess
-      w[i + 3][j + 3] = (sin((x[j] - .5) * M_PI * 2)) * 1e-6 + gs[i][j];
+      w[i + 3][j + 3] = (sin((x[j] - .5) * M_PI * 2.)) * 1e-6 + gs[i][j];
     }
   }
   matrixContinuation(w);
@@ -381,22 +374,20 @@ int main(int argc, char *argv[]) {
   while (err0 > errtol) {
 
     // Heun's predictor-corrector method
-    auto firstApprox = derf(w);
-    auto hamilton = hamiltonian(firstApprox.fxp, firstApprox.fxm,
-                                firstApprox.fyp, firstApprox.fym);
-    for (size_t i = 0; i < y.size(); i++) {
-      for (size_t j = 0; j < x.size(); j++) {
+    auto approx = derf(w);
+    auto hamilton = hamiltonian(approx.fxp, approx.fxm, approx.fyp, approx.fym);
+    for (size_t i = 0; i < y.size(); ++i) {
+      for (size_t j = 0; j < x.size(); ++j) {
         wnew0[i + 3][j + 3] = max(
             (1 - ll * dt) * w[i + 3][j + 3] - hamilton[i][j] * dt, gs[i][j]);
       }
     }
     matrixContinuation(wnew0);
 
-    auto secondApprox = derf(wnew0);
-    hamilton = hamiltonian(secondApprox.fxm, secondApprox.fxp, secondApprox.fym,
-                           secondApprox.fyp);
-    for (size_t i = 0; i < y.size(); i++) {
-      for (size_t j = 0; j < x.size(); j++) {
+    approx = derf(wnew0);
+    hamilton = hamiltonian(approx.fxp, approx.fxm, approx.fyp, approx.fym);
+    for (size_t i = 0; i < y.size(); ++i) {
+      for (size_t j = 0; j < x.size(); ++j) {
         wnew1[i + 3][j + 3] =
             max((1 - ll * dt) * wnew0[i + 3][j + 3] - hamilton[i][j] * dt,
                 gs[i][j]);
@@ -406,19 +397,29 @@ int main(int argc, char *argv[]) {
 
     auto wnew = halfSum(w, wnew1);
     err0 = 0;
-    for (size_t i = 0; i < y.size(); i++) {
-      for (size_t j = 0; j < x.size(); j++) {
+    for (size_t i = 0; i < y.size(); ++i) {
+      for (size_t j = 0; j < x.size(); ++j) {
         err0 = max(err0, abs(w[i + 3][j + 3] - wnew[i + 3][j + 3]));
       }
     }
+    err.push_back(err0);
+    if (!err.empty() && err0 > err.back()) {
+      cout << "AAAA" << "\n";
+      cout << "Steps: " << ncounter << "\n";
+      cout << "Error last: " << err.back() << "Current error: " << err0 << "\n";
+    }
     w = wnew;
     ncounter++;
-    err.push_back(err0);
+    if (ncounter % 100 == 0) {
+      cout << "Steps: " << ncounter << "\n";
+      cout << "Error: " << err.back() << "\n";
+    }
   }
   auto end = chrono::high_resolution_clock::now();
   double time_taken =
       1e-9 * chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-  cout << "Main calculation took: " << fixed << time_taken << setprecision(9)
+  cout << endl
+       << "Main calculation took: " << fixed << time_taken << setprecision(9)
        << " sec" << endl;
   cout << "Steps: " << ncounter << endl;
   cout << "Final error: " << err.back() << endl;
