ae65f26d882d343501efb5bfbd03ece4858124ca
Kaloian Stoilov <kaloan_sto@abv.bg>
2025-04-14 11:06:37 +0300

    Extract some code in functions

--- code/c++/mobility-heap/src/main.cpp
+++ code/c++/mobility-heap/src/main.cpp
@@ -6,6 +6,7 @@
 #include <format>
 #include <fstream>
 #include <iomanip>
+#include <ios>
 #include <iostream>
 // #include <iterator>
 #include <memory>
@@ -25,25 +26,42 @@ using Array4D = Array4DGeneric<double, n1, n2, n3, n4>;
 
 // parameters from Goldberg, Agusto 2021
 // human
-constexpr auto N1 = 1e6;
-constexpr auto N2 = 1e7;
-constexpr auto Gamma1 = 1. / 14.;
-constexpr auto Gamma2 = 1. / 14.;
+// constexpr auto N1 = 1e6;
+// constexpr auto N2 = 1e7;
+// constexpr auto Gamma1 = 1. / 14.;
+// constexpr auto Gamma2 = 1. / 14.;
+// constexpr auto BetaHV = 0.5;
+// constexpr auto p11 = 0.5;
+// constexpr auto p12 = 1 - p11;
+// constexpr auto p22 = 0.5;
+// constexpr auto p21 = 1 - p22;
+constexpr auto N1 = 9.38e6;
+constexpr auto N2 = 4.47e6;
+constexpr auto Gamma1 = 0.063;
+constexpr auto Gamma2 = 0.058;
 constexpr auto BetaHV = 0.5;
-constexpr auto p11 = 0.5;
+constexpr auto p11 = 0.78;
 constexpr auto p12 = 1 - p11;
-constexpr auto p22 = 0.5;
+constexpr auto p22 = 0.7;
 constexpr auto p21 = 1 - p22;
+
 constexpr auto H1 = p11 * N1 + p21 * N2;
 constexpr auto H2 = p21 * N1 + p22 * N2;
 
 // mosquito
-constexpr auto M1 = 1e7;
-constexpr auto M2 = 1e8;
-constexpr auto mu1 = 1. / 20.;
-constexpr auto mu2 = 1. / 20.;
-constexpr auto a1 = 1.;
-constexpr auto a2 = 1.;
+// constexpr auto M1 = 1e7;
+// constexpr auto M2 = 1e8;
+// constexpr auto mu1 = 1. / 20.;
+// constexpr auto mu2 = 1. / 20.;
+// constexpr auto a1 = 1.;
+// constexpr auto a2 = 1.;
+constexpr auto M1 = 1.73e7;
+constexpr auto M2 = 3e7;
+constexpr auto mu1 = 0.032;
+constexpr auto mu2 = 0.047;
+constexpr auto a1 = 0.158;
+constexpr auto a2 = 0.160;
+
 constexpr auto BetaVH = 0.1;
 constexpr auto tau = 10;
 
@@ -58,16 +76,19 @@ constexpr auto c21 = BetaHV * p12 * a2 * N1 / H2;
 constexpr auto c22 = BetaHV * p22 * a2 * N2 / H2;
 
 // maximum infected
-constexpr auto Imax1 = .1;
-constexpr auto Imax2 = .1;
+constexpr auto Imax1 = .15;
+constexpr auto Imax2 = .15;
 // set to reduce the size of the domain where the viability kernel lies
-constexpr auto ymax1 = .5;
-constexpr auto ymax2 = .5;
+constexpr auto ymax1 = 1.;
+constexpr auto ymax2 = 1.;
 
 // control
-constexpr auto k = .6;
-constexpr auto umax1 = .3;
-constexpr auto umax2 = .4;
+// constexpr auto k = .6;
+// constexpr auto umax1 = .3;
+// constexpr auto umax2 = .4;
+constexpr auto k = .35;
+constexpr auto umax1 = .20;
+constexpr auto umax2 = .12;
 
 // threshold for I at maximum control
 // const auto Is =
@@ -75,18 +96,15 @@ constexpr auto umax2 = .4;
 //     (alpha * Beta * nu * (1 - k * umax) * (1 - k * umax) +
 //      Beta * Gamma * (1 - k * umax));
 
-// predefine
-constexpr auto errtol = 1e-10;
-
 // lambda - lipschitz constant for the system
 const auto Lexact = 3. * (b11 + b12 + b21 + b22 + c11 + c12 + c21 + c22) +
                     Gamma1 + Gamma2 + mu1 + mu2;
 const auto L = 1.05 * Lexact;
 
 // numerical space step
-constexpr auto Nx1 = 35;
+constexpr auto Nx1 = 25;
 constexpr auto dx1 = Imax1 / (Nx1 - 1);
-constexpr auto Nx2 = 35;
+constexpr auto Nx2 = 25;
 constexpr auto dx2 = Imax2 / (Nx2 - 1);
 constexpr auto Ny1 = 25;
 constexpr auto dy1 = ymax1 / (Ny1 - 1);
@@ -152,40 +170,21 @@ unique_ptr<Array4D<x1.size(), x2.size(), y1.size(), y2.size()>>
     hamiltonian(nullptr);
 
 template <size_t n1, size_t n2, size_t n3, size_t n4>
-Array4D<n1, n2, n3, n4>
-    __always_inline weightedSum(const unique_ptr<Array4D<n1, n2, n3, n4>> &mat1,
-                                const unique_ptr<Array4D<n1, n2, n3, n4>> &mat2,
-                                const double &w1, const double &w2) noexcept {
-  Array4D<n1, n2, n3, n4> res;
+__always_inline void
+weightedSum(const unique_ptr<Array4D<n1, n2, n3, n4>> &res,
+            const unique_ptr<Array4D<n1, n2, n3, n4>> &mat1,
+            const unique_ptr<Array4D<n1, n2, n3, n4>> &mat2, const double &w1,
+            const double &w2) noexcept {
   for (size_t i1 = 0; i1 < n1; ++i1) {
     for (size_t i2 = 0; i2 < n2; ++i2) {
       for (size_t i3 = 0; i3 < n3; ++i3) {
         for (size_t i4 = 0; i4 < n4; ++i4) {
-          res[i1][i2][i3][i4] =
+          (*res)[i1][i2][i3][i4] =
               w1 * (*mat1)[i1][i2][i3][i4] + w2 * (*mat2)[i1][i2][i3][i4];
         }
       }
     }
   }
-  return res;
-}
-
-template <size_t n1, size_t n2, size_t n3, size_t n4>
-__always_inline Array4D<n1, n2, n3, n4>
-halfSum(const Array4D<n1, n2, n3, n4> &mat1,
-        const Array4D<n1, n2, n3, n4> &mat2) noexcept {
-  Array4D<n1, n2, n3, n4> res;
-  for (size_t i1 = 0; i1 < n1; ++i1) {
-    for (size_t i2 = 0; i2 < n2; ++i2) {
-      for (size_t i3 = 0; i3 < n3; ++i3) {
-        for (size_t i4 = 0; i4 < n4; ++i4) {
-          res[i1][i2][i3][i4] =
-              (mat1[i1][i2][i3][i4] + mat2[i1][i2][i3][i4]) / 2.;
-        }
-      }
-    }
-  }
-  return res;
 }
 
 template <size_t n1, size_t n2, size_t n3, size_t n4>
@@ -196,9 +195,11 @@ __always_inline void printSolution(const Array4D<n1, n2, n3, n4> &arr,
     for (size_t i2 = 0; i2 < Nx2; ++i2) {
       for (size_t j1 = 0; j1 < Ny1; ++j1) {
         for (size_t j2 = 0; j2 < Ny2; ++j2) {
+          stream << fixed << setprecision(8);
           stream << x1[i1] << delim << x2[i2] << delim << y1[j1] << delim
-                 << y2[j2] << delim
-                 << arr[i1 + padX1][i2 + padX2][j1 + padY1][j2 + padY2] << "\n";
+                 << y2[j2] << delim;
+          stream << scientific;
+          stream << arr[i1 + padX1][i2 + padX2][j1 + padY1][j2 + padY2] << "\n";
         }
       }
     }
@@ -309,31 +310,31 @@ matrixContinuation(const unique_ptr<Array4D<n1, n2, n3, n4>> &mat) noexcept {
 double derf(const double &v1, const double &v2, const double &v3,
             const double &v4, const double &v5) noexcept {
   // smoothness of stencils
-  auto S1 = 13. / 12. * (v1 - 2. * v2 + v3) * (v1 - 2. * v2 + v3) +
-            1. / 4. * (v1 - 4. * v2 + 3. * v3) * (v1 - 4. * v2 + 3. * v3);
-  auto S2 = 13. / 12. * (v2 - 2. * v3 + v4) * (v2 - 2. * v3 + v4) +
-            1. / 4. * (v2 - v4) * (v2 - v4);
-  auto S3 = 13. / 12. * (v3 - 2. * v4 + v5) * (v3 - 2. * v4 + v5) +
-            1. / 4. * (3. * v3 - 4. * v4 + v5) * (3. * v3 - 4. * v4 + v5);
+  const auto S1 = 13. / 12. * (v1 - 2. * v2 + v3) * (v1 - 2. * v2 + v3) +
+                  1. / 4. * (v1 - 4. * v2 + 3. * v3) * (v1 - 4. * v2 + 3. * v3);
+  const auto S2 = 13. / 12. * (v2 - 2. * v3 + v4) * (v2 - 2. * v3 + v4) +
+                  1. / 4. * (v2 - v4) * (v2 - v4);
+  const auto S3 = 13. / 12. * (v3 - 2. * v4 + v5) * (v3 - 2. * v4 + v5) +
+                  1. / 4. * (3. * v3 - 4. * v4 + v5) * (3. * v3 - 4. * v4 + v5);
   // find the maximum of vi's
-  auto maxv2 = max({v1 * v1, v2 * v2, v3 * v3, v4 * v4, v5 * v5});
-  auto eps1 = 1e-6 * maxv2 + 1e-99;
+  const auto maxv2 = max({v1 * v1, v2 * v2, v3 * v3, v4 * v4, v5 * v5});
+  const auto eps1 = 1e-6 * maxv2 + 1e-99;
   // weights
-  auto a1 = .1 / ((S1 + eps1) * (S1 + eps1));
-  auto a2 = .6 / ((S2 + eps1) * (S2 + eps1));
-  auto a3 = .3 / ((S3 + eps1) * (S3 + eps1));
-  auto w1 = a1 / (a1 + a2 + a3);
-  auto w2 = a2 / (a1 + a2 + a3);
-  auto w3 = a3 / (a1 + a2 + a3);
-  auto f1x = v1 / 3. - 7. * v2 / 6 + 11. * v3 / 6.;
-  auto f2x = -v2 / 6. + 5. * v3 / 6. + v4 / 3.;
-  auto f3x = v3 / 3. + 5. * v4 / 6. - v5 / 6.;
+  const auto a1 = .1 / ((S1 + eps1) * (S1 + eps1));
+  const auto a2 = .6 / ((S2 + eps1) * (S2 + eps1));
+  const auto a3 = .3 / ((S3 + eps1) * (S3 + eps1));
+  const auto w1 = a1 / (a1 + a2 + a3);
+  const auto w2 = a2 / (a1 + a2 + a3);
+  const auto w3 = a3 / (a1 + a2 + a3);
+  const auto f1x = v1 / 3. - 7. * v2 / 6 + 11. * v3 / 6.;
+  const auto f2x = -v2 / 6. + 5. * v3 / 6. + v4 / 3.;
+  const auto f3x = v3 / 3. + 5. * v4 / 6. - v5 / 6.;
   return w1 * f1x + w2 * f2x + w3 * f3x;
 }
 
 template <size_t n1, size_t n2, size_t n3, size_t n4>
-__always_inline void
-derf(const unique_ptr<Array4D<n1, n2, n3, n4>> &mat) noexcept {
+__always_inline void approximate_derivatives(
+    const unique_ptr<Array4D<n1, n2, n3, n4>> &mat) noexcept {
   for (size_t i1 = 0; i1 < n1 - 1; ++i1) {
     for (size_t i2 = 0; i2 < n2 - 6; ++i2) {
       for (size_t i3 = 0; i3 < n3 - 6; ++i3) {
@@ -441,9 +442,10 @@ __always_inline void calcHamiltonian() noexcept {
     for (size_t i2 = 0; i2 < Nx2; ++i2) {
       for (size_t i3 = 0; i3 < Ny1; ++i3) {
         for (size_t i4 = 0; i4 < Ny2; ++i4) {
+          auto hamilt = 0.;
+
           const auto fx1p = (*Fx1p)[i1][i2][i3][i4];
           const auto fx1m = (*Fx1m)[i1][i2][i3][i4];
-          auto hamilt = 0.;
           const auto chooseAx1 =
               (*wwx1)[i1][i2][i3][i4] * (fx1p * fx1m < 1e-99);
           const auto intermX1 =
@@ -506,13 +508,13 @@ __always_inline void
 performTimeStep(const unique_ptr<Array4D<n1, n2, n3, n4>> &tp1Matrix,
                 const unique_ptr<Array4D<n1, n2, n3, n4>> &tMatrix,
                 const double &dt) noexcept {
-  derf(tMatrix);
+  approximate_derivatives(tMatrix);
   calcHamiltonian();
 
-  for (size_t i1 = 0; i1 < n1 - padX1; ++i1) {
-    for (size_t i2 = 0; i2 < n2 - padX2; ++i2) {
-      for (size_t j1 = 0; j1 < n3 - padY1; ++j1) {
-        for (size_t j2 = 0; j2 < n4 - padY2; ++j2) {
+  for (size_t i1 = 0; i1 < n1 - 2 * padX1; ++i1) {
+    for (size_t i2 = 0; i2 < n2 - 2 * padX2; ++i2) {
+      for (size_t j1 = 0; j1 < n3 - 2 * padY1; ++j1) {
+        for (size_t j2 = 0; j2 < n4 - 2 * padY2; ++j2) {
           (*tp1Matrix)[i1 + 3][i2 + 3][j1 + 3][j2 + 3] =
               max((1 - L * dt) * (*tMatrix)[i1 + 3][i2 + 3][j1 + 3][j2 + 3] -
                       (*hamiltonian)[i1][i2][j1][j2] * dt,
@@ -525,24 +527,7 @@ performTimeStep(const unique_ptr<Array4D<n1, n2, n3, n4>> &tp1Matrix,
   matrixContinuation(tp1Matrix);
 }
 
-int main(int argc, char *argv[]) {
-  // generate(x1.begin(), x1.end(),
-  //          [n = 0]() mutable { return (-padX1 + n++) * dx1; });
-  // x1[padX1] = 0;
-  // x1[padX1 + Nx1 - 1] = Imax1;
-  // generate(y1.begin(), y1.end(),
-  //          [n = 0]() mutable { return (-padY1 + n++) * dy1; });
-  // y1[padY1] = 0;
-  // y1[padY1 + Ny1 - 1] = ymax1;
-  // generate(x2.begin(), x2.end(),
-  //          [n = 0]() mutable { return (-padX2 + n++) * dx2; });
-  // x2[padX2] = 0;
-  // x2[padX2 + Nx2 - 1] = Imax2;
-  // generate(y2.begin(), y2.end(),
-  //          [n = 0]() mutable { return (-padY2 + n++) * dy2; });
-  // y2[padY2] = 0;
-  // y2[padY2 + Ny2 - 1] = ymax2;
-
+void initialize() {
   hx1 = make_unique<Array4D<x1.size(), x2.size(), y1.size(), y2.size()>>();
   hx2 = make_unique<Array4D<x1.size(), x2.size(), y1.size(), y2.size()>>();
   hy1 = make_unique<Array4D<x1.size(), x2.size(), y1.size(), y2.size()>>();
@@ -574,6 +559,23 @@ int main(int argc, char *argv[]) {
 
   distSgnd = make_unique<Array4D<x1.size(), x2.size(), y1.size(), y2.size()>>();
 
+  // generate(x1.begin(), x1.end(),
+  //          [n = 0]() mutable { return (-padX1 + n++) * dx1; });
+  // x1[padX1] = 0;
+  // x1[padX1 + Nx1 - 1] = Imax1;
+  // generate(y1.begin(), y1.end(),
+  //          [n = 0]() mutable { return (-padY1 + n++) * dy1; });
+  // y1[padY1] = 0;
+  // y1[padY1 + Ny1 - 1] = ymax1;
+  // generate(x2.begin(), x2.end(),
+  //          [n = 0]() mutable { return (-padX2 + n++) * dx2; });
+  // x2[padX2] = 0;
+  // x2[padX2 + Nx2 - 1] = Imax2;
+  // generate(y2.begin(), y2.end(),
+  //          [n = 0]() mutable { return (-padY2 + n++) * dy2; });
+  // y2[padY2] = 0;
+  // y2[padY2 + Ny2 - 1] = ymax2;
+
   generate(x1.begin(), x1.end(), [n = 0]() mutable { return (n++) * dx1; });
   x1[Nx1 - 1] = Imax1;
   generate(x2.begin(), x2.end(), [n = 0]() mutable { return (n++) * dx2; });
@@ -582,12 +584,45 @@ int main(int argc, char *argv[]) {
   y1[Ny1 - 1] = ymax1;
   generate(y2.begin(), y2.end(), [n = 0]() mutable { return (n++) * dy2; });
   y2[Ny2 - 1] = ymax2;
+}
+
+template <size_t n1, size_t n2, size_t n3, size_t n4>
+double max_abs_diff(const unique_ptr<Array4D<n1, n2, n3, n4>> &mat1,
+                    const unique_ptr<Array4D<n1, n2, n3, n4>> &mat2) {
+  auto res = 0.;
+  // auto solSize = 0.;
+  for (size_t i1 = 0; i1 < x1.size(); ++i1) {
+    for (size_t i2 = 0; i2 < x2.size(); ++i2) {
+      for (size_t j1 = 0; j1 < y1.size(); ++j1) {
+        for (size_t j2 = 0; j2 < y2.size(); ++j2) {
+          res = max(res, abs((*mat1)[i1 + 3][i2 + 3][j1 + 3][j2 + 3] -
+                             (*mat2)[i1 + 3][i2 + 3][j1 + 3][j2 + 3]));
+          // solSize += abs((*wnew0)[i1 + 3][i2 + 3][j1 + 3][j2 + 3]);
+          // currentError = max(currentError,
+          //                    abs((*w)[i1 + 3][i2 + 3][j1 + 3][j2 + 3] -
+          //                        (*wnew0)[i1 + 3][i2 + 3][j1 + 3][j2 +
+          //                        3]));
+
+          // currentError = max(currentError,
+          //                    abs(((*w)[i1 + 3][i2 + 3][j1 + 3][j2 + 3] -
+          //                         (*wnew0)[i1 + 3][i2 + 3][j1 + 3][j2 + 3])
+          //                         /
+          //                        (*w)[i1 + 3][i2 + 3][j1 + 3][j2 + 3]));
+        }
+      }
+    }
+  }
+  return res;
+}
+
+int main(int argc, char *argv[]) {
+
+  initialize();
 
   array<double, 4> mpd = {0, 0, 0, 0};
 
-  const auto w =
-      make_unique<Array4D<x1.size() + 2 * padX1, x2.size() + 2 * padX2,
-                          y1.size() + 2 * padY1, y2.size() + 2 * padY2>>();
+  auto w = make_unique<Array4D<x1.size() + 2 * padX1, x2.size() + 2 * padX2,
+                               y1.size() + 2 * padY1, y2.size() + 2 * padY2>>();
   wx1 = make_unique<
       Array4D<x1.size() + 2 * padX1 - 1, x2.size(), y1.size(), y2.size()>>();
   wx2 = make_unique<
@@ -648,7 +683,12 @@ int main(int argc, char *argv[]) {
 
           // set signed distance
           (*distSgnd)[i1][i2][j1][j2] =
-              -min(abs(x1[i1] - Imax1), abs(x2[i2] - Imax2));
+              -min({abs(x1[i1] - Imax1), abs(x2[i2] - Imax2)});
+          // -min({abs(x1[i1]), abs(x2[i2]), abs(x1[i1] - Imax1),
+          //       abs(x2[i2] - Imax2)});
+          // -min({abs(x1[i1]), abs(x2[i2]), abs(y1[j1]), abs(y2[j2]),
+          //       abs(x1[i1] - Imax1), abs(x2[i2] - Imax2), abs(y1[j1] - 1),
+          //       abs(y2[j2] - 1)});
           // -__builtin_sqrt((x1[i1] - Imax1) * (x1[i1] - Imax1) +
           //                 (x2[i2] - Imax2) * (x2[i2] - Imax2));
 
@@ -675,43 +715,34 @@ int main(int argc, char *argv[]) {
       0.9 / max(mpd[0] / dx1 + mpd[1] / dx2 + mpd[2] / dy1 + mpd[3] / dy2, L);
 
   matrixContinuation(w);
-  const auto wnew0 =
+  auto wnew0 =
       make_unique<Array4D<x1.size() + 2 * padX1, x2.size() + 2 * padX2,
                           y1.size() + 2 * padY1, y2.size() + 2 * padY2>>();
-  const auto wnew1 =
-      make_unique<Array4D<x1.size() + 2 * padX1, x2.size() + 2 * padX2,
-                          y1.size() + 2 * padY1, y2.size() + 2 * padY2>>();
-  const auto wnew =
+  auto wnew1 =
       make_unique<Array4D<x1.size() + 2 * padX1, x2.size() + 2 * padX2,
                           y1.size() + 2 * padY1, y2.size() + 2 * padY2>>();
 
-  auto currentError = 1.;
+  constexpr auto errtol = 1e-8;
+  auto currentError = errtol;
   vector<double> err;
   auto ncounter = 0;
   auto start = chrono::high_resolution_clock::now();
 
-  while (currentError > errtol) {
+  while (currentError >= errtol) {
 
-    // Heun's predictor-corrector method
+    // Heun's predictor-corrector method 2nd order
+    // performTimeStep(wnew0, w, dt);
+    // performTimeStep(wnew1, wnew0, dt);
+    // weightedSum(wnew0, w, wnew1, 1. / 2., 1. / 2.);
+
+    // Heun's predictor-corrector method 3rd order
     performTimeStep(wnew0, w, dt);
     performTimeStep(wnew1, wnew0, dt);
-    *wnew0 = weightedSum(w, wnew1, 3. / 4., 1. / 4.);
+    weightedSum(wnew0, w, wnew1, 3. / 4., 1. / 4.);
     performTimeStep(wnew1, wnew0, dt);
-    auto wnew = weightedSum(w, wnew1, 1. / 3., 2. / 3.);
-    // const auto wnew = halfSum(w, wnew1);
+    weightedSum(wnew0, w, wnew1, 1. / 3., 2. / 3.);
 
-    currentError = 0;
-    for (size_t i1 = 0; i1 < x1.size(); ++i1) {
-      for (size_t i2 = 0; i2 < x2.size(); ++i2) {
-        for (size_t j1 = 0; j1 < y1.size(); ++j1) {
-          for (size_t j2 = 0; j2 < y2.size(); ++j2) {
-            currentError =
-                max(currentError, abs((*w)[i1 + 3][i2 + 3][j1 + 3][j2 + 3] -
-                                      wnew[i1 + 3][i2 + 3][j1 + 3][j2 + 3]));
-          }
-        }
-      }
-    }
+    currentError = max_abs_diff(w, wnew0);
     err.push_back(currentError);
     if (!err.empty() && currentError > err.back()) {
       cout << "AAAA" << "\n";
@@ -719,9 +750,9 @@ int main(int argc, char *argv[]) {
       cout << "Error last: " << err.back() << "Current error: " << currentError
            << "\n";
     }
-    *w = wnew;
+    w.swap(wnew0);
     ncounter++;
-    if (ncounter % 50 == 0) {
+    if (ncounter % 5 == 0) {
       cout << "Steps: " << ncounter << "\n";
       cout << "Error: " << err.back() << "\n";
       cout << "Time: "
